fd49ce7218ff354b3bbabe7ca199e7a63796b717
mesca <github@clisson.org>
2019-03-15 17:59:17 +0100

    Include static data

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ with open('timeflux_ui/__init__.py') as f:
 setup(
     name='Timeflux UI',
     packages=find_packages(),
-    include_package_data=True,
     version=VERSION,
     description='Timeflux monitoring web interface.',
     long_description=DESCRIPTION,
